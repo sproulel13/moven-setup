426328acc96786f16cfefa2fb1ca8e6b7de2e4b3
sproulel13 <lucas@moven.com>
2019-02-15 11:43:36 -0500

    setupfiles for moven

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkidr $HOME/workspace
+cd $HOME/workspace
+git clone https://github.com/moven/dev-tools
+git clone https://github.com/moven/moven
+git clone https://github.com/moven/moven-app
+git clone https://github.com/moven/moven-web
+git clone https://github.com/moven/admin
+git clone https://github.com/moven/integration-api
+git clone https://github.com/moven/authserver
+git clone https://github.com/moven/integration-api-producer
+git clone https://github.com/moven/push-notification-service
+git clone https://github.com/moven/real-time-transaction-notifier
+cd ./dev-tools/utils/new-dev
+sh ./new-mac-setup.sh
